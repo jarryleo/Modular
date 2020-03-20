f336363d9b3ebde33ba6d90d55ca37042c63bc6c
lingluo <lingluo511@gmail.com>
2020-03-20 20:04:30 +0800

    [add] 接口日志库增加请求参数增加大小判断，防止上传文件打印导致内存暴增

--- module_frame/src/main/java/cn/leo/frame/network/interceptor/LoggerInterceptor.kt
+++ module_frame/src/main/java/cn/leo/frame/network/interceptor/LoggerInterceptor.kt
@@ -1,8 +1,9 @@
 package cn.leo.frame.network.interceptor
 
 
-import android.util.Log
 import cn.leo.frame.network.sign.UrlUtil
+import cn.leo.z_logger.Logger
+import cn.leo.z_logger.toLogE
 import okhttp3.Interceptor
 import okhttp3.RequestBody
 import okhttp3.Response
@@ -14,15 +15,15 @@ import java.nio.charset.Charset
 /**
  * @author lingluo
  */
-class LoggerInterceptor : Interceptor {
+class LoggergerInterceptor : Interceptor {
 
     @Synchronized
     @Throws(IOException::class)
     override fun intercept(chain: Interceptor.Chain): Response {
         val request = chain.request()
         val startTime = System.nanoTime()
-        Log.d(TAG, "----------Start----------")
-        Log.d(TAG, String.format("请求地址：| %s", request.url))
+        Logger.d(TAG, "----------Start----------")
+        Logger.d(TAG, String.format("请求地址：| %s", request.url))
         //执行请求
         val response = chain.proceed(request)
         printParams(request.body)
@@ -32,18 +33,18 @@ class LoggerInterceptor : Interceptor {
         // Buffer the entire body.
         source.request(Integer.MAX_VALUE.toLong())
         val buffer = source.buffer()
-        //接口数据大于16K不显示
+        //接口数据大于32K不显示
         if (buffer.size > 1024 * 32) {
-            Log.d(
+            Logger.d(
                 TAG, UrlUtil.getBaseUrl(request.url.toString())
                         + "请求返回：| (长度:" + buffer.size
-                        + ")大于16K不打印,点击链接在网页查看"
+                        + ")大于32K不打印,点击链接在网页查看"
             )
             return response
         } else {
             val headers = response.headers
             val header = headers.toString()
-            Log.d(TAG, String.format("headers：|\n%s\n", header))
+            Logger.d(TAG, String.format("headers：|\n%s\n", header))
 
             var charset: Charset? = Charset.defaultCharset()
             val contentType = body.contentType()
@@ -52,17 +53,21 @@ class LoggerInterceptor : Interceptor {
             }
             val bodyString = buffer.clone().readString(charset!!)
             //String str = URLDecoder.decode(bodyString, "UTF-8");
-            Log.d(TAG, String.format("请求返回：| %s", bodyString))
+            Logger.d(TAG, String.format("请求返回：| %s", bodyString))
         }
         val endTime = System.nanoTime()
         val duration = (endTime - startTime) / 1e6
-        Log.d(TAG, "----------End:请求耗时:" + duration + "毫秒----------")
+        Logger.d(TAG, "----------End:请求耗时:" + duration + "毫秒----------")
         return response
     }
 
     @Throws(IOException::class)
     private fun printParams(body: RequestBody?) {
         if (body == null) {
+            return
+        }
+        if (body.contentLength() > 1024 * 32) {
+            "请求参数大于16K不打印".toLogE(TAG)
             return
         }
         val buffer = Buffer()
@@ -73,10 +78,10 @@ class LoggerInterceptor : Interceptor {
             charset = contentType.charset(charset)
         }
         val params = buffer.readString(charset!!)
-        Log.d(TAG, "请求参数：| $params")
+        Logger.d(TAG, "请求参数：| $params")
     }
 
     companion object {
-        var TAG = "LoggerInterceptor"
+        var TAG = "LoggergerInterceptor"
     }
 }
